3de19604d377343ac16fe3c88f74d23a3ce8cd02
bhuwankumar-design <bhuwan.kumar@rungta.org>
2026-06-06 21:26:58 +0530

    Update PayrollSystem.java

--- PayrollSystem.java
+++ PayrollSystem.java
@@ -1,20 +1,12 @@
-// Parent Class
-class Employee {
+abstract class Employee {
     String name;
     int id;
-    double baseSalary;
 
-    // Constructor
-    Employee(String name, int id, double baseSalary) {
+    Employee(String name, int id) {
         this.name = name;
         this.id = id;
-        this.baseSalary = baseSalary;
     }
-
-    // Method (to be overridden)
-    double calculateSalary() {
-        return baseSalary;
-    }
+    abstract double calculateSalary();
 
     void displayDetails() {
         System.out.println("Name: " + name);
@@ -22,54 +14,74 @@ class Employee {
     }
 }
 
-// Full-Time Employee
 class FullTimeEmployee extends Employee {
+    double baseSalary;
     double bonus;
 
     FullTimeEmployee(String name, int id, double baseSalary, double bonus) {
-        super(name, id, baseSalary);
+        super(name, id);
+        this.baseSalary = baseSalary;
         this.bonus = bonus;
     }
-
-    @Override
     double calculateSalary() {
         return baseSalary + bonus;
     }
+
+    void conductMeeting() {
+        System.out.println("Meeting Conducted");
+    }
 }
 
-// Part-Time Employee
 class PartTimeEmployee extends Employee {
     int hoursWorked;
     double hourlyRate;
 
     PartTimeEmployee(String name, int id, int hoursWorked, double hourlyRate) {
-        super(name, id, 0); // baseSalary not needed here
+        super(name, id);
         this.hoursWorked = hoursWorked;
         this.hourlyRate = hourlyRate;
     }
-
-    @Override
     double calculateSalary() {
         return hoursWorked * hourlyRate;
     }
+
+    void showHours() {
+        System.out.println("Hours Worked: " + hoursWorked);
+    }
 }
 
-// Main Class
 public class PayrollSystem {
     public static void main(String[] args) {
 
-        // Creating objects using polymorphism
         Employee e1 = new FullTimeEmployee("Rahul", 101, 40000, 10000);
         Employee e2 = new PartTimeEmployee("Aman", 102, 80, 150);
 
-        // Full-Time Employee Output
-        System.out.println("----- Full Time Employee -----");
-        e1.displayDetails();
-        System.out.println("Salary: " + e1.calculateSalary());
+        Employee[] employees = {e1, e2};
 
-        // Part-Time Employee Output
-        System.out.println("\n----- Part Time Employee -----");
-        e2.displayDetails();
-        System.out.println("Salary: " + e2.calculateSalary());
+        for (Employee emp : employees) {
+
+            emp.displayDetails();
+
+            double salary = emp.calculateSalary();
+            System.out.println("Salary: " + salary);
+
+            if (salary > 30000) {
+                System.out.println("High Salary Employee");
+            } else {
+                System.out.println("Regular Employee");
+            }
+
+            if (emp instanceof FullTimeEmployee) {
+                FullTimeEmployee ft =
+                        (FullTimeEmployee) emp;
+                ft.conductMeeting();
+            } else if (emp instanceof PartTimeEmployee) {
+                PartTimeEmployee pt =
+                        (PartTimeEmployee) emp;
+                pt.showHours();
+            }
+
+            System.out.println(" ");
+        }
     }
 }
